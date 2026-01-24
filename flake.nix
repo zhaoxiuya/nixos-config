3521efb8aeb9d2d88aa02f73c7aa9c575d51d2a9
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-24 23:16:07 +0900

    flake

--- flake.nix
+++ flake.nix
@@ -3,22 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        nixosConfigurations = {
-          nixos = pkgs.nixosSystem {
-            inherit system;
-            modules = [ ./configuration.nix ];
-          };
-        };
-      }
-    );
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux"; 
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
 }
-
