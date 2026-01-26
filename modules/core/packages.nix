2c56bfebbd30be7e7571bb4264dea8c35f3bae3a
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 01:49:59 +0900

    fixed kvantum

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -3,5 +3,8 @@
 {
   environment.systemPackages = with pkgs; [
     vim neovim neofetch wget git openssh tree
+    kdePackages.spectacle
+    kdePackages.qtstyleplugin-kvantum
+    libsForQt5.qtstyleplugin-kvantum
   ];
 }
