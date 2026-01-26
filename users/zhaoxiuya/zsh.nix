b6e05a23a35146c70e592b8f3319ae563eacc260
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 01:30:24 +0900

    test zsh

--- users/zhaoxiuya/zsh.nix
+++ users/zhaoxiuya/zsh.nix
@@ -6,29 +6,5 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
-    plugins = [
-      {
-        name = "pure";
-        src = pkgs.pure-prompt;
-        file = "share/zsh/site-functions/async.zsh";
-      }
-      {
-        name = "pure";
-        src = pkgs.pure-prompt;
-        file = "share/zsh/site-functions/pure.zsh";
-      }
-    ];
-    initContent = ''
-      source ${pkgs.pure-prompt}/share/zsh/site-functions/async.zsh
-      autoload -U promptinit; promptinit
-      fpath+=( "${pkgs.pure-prompt}/share/zsh/site-functions" )
-      prompt pure
-    '';
-
-    shellAliases = {
-      ll = "ls -l";
-      snrs = "sudo nixos-rebuild switch --flake .#iridium";
-    };
   };
 }
