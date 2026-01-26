e9d3cfda9888d94dc8b6ae6c25c87ed0c808dc28
zhaoxiuya <zhaoxiuya12@gmail.com>
2026-01-26 23:55:22 +0900

    flake

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -8,7 +8,7 @@ in
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
 #  services.displayManager.sddm.theme = null;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   services.xserver.xkb = {
     layout = "us";
