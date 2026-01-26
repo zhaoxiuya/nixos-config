fbf551f5635c99d8c3f15eb4a281c06a854e5c1b
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 01:23:25 +0900

    stylix

--- users/zhaoxiuya/stylix.nix
+++ users/zhaoxiuya/stylix.nix
@@ -5,7 +5,7 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
-    stylix.fonts = {
+    fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrainsMono Nerd Font";
