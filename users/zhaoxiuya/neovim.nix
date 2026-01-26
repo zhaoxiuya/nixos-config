8f2f94beb858e5939d9b36eee0b16a1c293ec7a4
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 00:26:13 +0900

    fixed nvim

--- users/zhaoxiuya/neovim.nix
+++ users/zhaoxiuya/neovim.nix
@@ -9,7 +9,7 @@
       vim.opt.shiftwidth = 2
       vim.opt.tabstop = 2
       vim.opt.softtabstop = 2
-      vim.opt.smartident = true
+      vim.opt.smartindent = true
       vim.opt.number = true
     '';
   };
