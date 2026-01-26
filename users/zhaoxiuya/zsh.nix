9d78fa272ee7211a6e14d4ae7901f6836374ba45
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 01:35:48 +0900

    test zsh

--- users/zhaoxiuya/zsh.nix
+++ users/zhaoxiuya/zsh.nix
@@ -6,5 +6,15 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "kubectl"
+        "helm"
+        "docker"
+      ];
+    };
   };
 }
