16bd3e822d47c560eb69bc1f1d43d4c290884ba8
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 17:58:58 +0900

    added gsnrs

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,8 +1,5 @@
-{
-  pkgs,
-  config,
-  ...
-}:
+{ pkgs, config,... }:
+
 {
   nix = {
     package = pkgs.nix;
@@ -12,4 +9,16 @@
     ];
   };
   nixpkgs.config.allowUnfree = true;
+  environment.interactiveShellInit = ''
+    gsnrs() {
+      if [ -z "$1" ]; then
+        echo "please write commit message"
+        return 1
+      fi
+
+      git add .
+      git commit -m "$1"
+      sudo nixos-rebuild switch --flake .#iridium
+    }
+  '';
 }
