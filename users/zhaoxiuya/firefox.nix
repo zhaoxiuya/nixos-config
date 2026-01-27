71fd5c70a86eee672782799b6046866b07791c15
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 20:03:45 +0900

    firefox

--- users/zhaoxiuya/firefox.nix
+++ users/zhaoxiuya/firefox.nix
@@ -3,13 +3,13 @@
 {
   programs.firefox = {
     enable = true;
-    profiles.zhaoxiuya = {
-      isDefault = true;
-      #path = "zhaoxiuya";
-      settings = {
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      };
-      extensions.force = true;
-    };
+#    profiles.zhaoxiuya = {
+#      isDefault = true;
+#      #path = "zhaoxiuya";
+#      settings = {
+#        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+#      };
+#      extensions.force = true;
+#    };
   };
 }
