702f728fb16ed517f28a5c56fe38c0ee73eb1151
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 17:08:10 +0900

    home

--- users/zhaoxiuya/home.nix
+++ users/zhaoxiuya/home.nix
@@ -6,16 +6,14 @@
     ./stylix
     ./neovim.nix
     ./git.nix
-    # ./bash.nix
     ./zsh.nix
     ./kitty.nix
   ];
 
-  home.stateVersion = "25.11";
-
   home.packages = with pkgs; [
     discord
     zed-editor
-    nerd-fonts.monoid
   ];
+
+  home.stateVersion = "25.11";
 }
