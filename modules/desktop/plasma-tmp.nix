76e8fffafc2824c7270e1bcafa7701531501f23b
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-27 17:03:12 +0900

    fmted

--- modules/desktop/plasma-tmp.nix
+++ modules/desktop/plasma-tmp.nix
@@ -5,7 +5,7 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  services.xserver.xkb = { 
+  services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
