90b90103728af65936a5ea1dc61dc565ba457ebd
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-28 05:41:12 +0900

    libvirtd

--- users/zhaoxiuya/winapps/default.nix
+++ users/zhaoxiuya/winapps/default.nix
@@ -13,10 +13,10 @@ in
   ];
 
   home.file.".config/winapps/winapps.conf".text = ''
-    WA_FLAVOR="rdp"
-    WA_RDP_USER="zhaoxiuya"
-    WA_RDP_PASS="1234"
-    WA_RDP_IP="192.168.122.105"
-    WA_VM_NAME="win10"
+    RDP_USER="zhaoxiuya"
+    RDP_PASS="1234"
+    RDP_IP="192.168.122.105"
+    VM_NAME="win10"
+    WAFLAVOR="libvirt"
   '';
 }
