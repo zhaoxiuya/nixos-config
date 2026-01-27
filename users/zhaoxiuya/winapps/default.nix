3f647d697764ead5ae7ff009e6709caf74af0d8d
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-28 02:43:14 +0900

    winapps

--- users/zhaoxiuya/winapps/default.nix
+++ users/zhaoxiuya/winapps/default.nix
@@ -12,7 +12,7 @@ in
   ];
 
   home.file.".config/winapps/winapps.conf".text = ''
-    WA_FLAVOR="libvirt"
+    WA_FLAVOR=""
     WA_RDP_USER="zhaoxiuya"
     WA_RDP_PASS="1234"
     WA_RDP_IP="192.168.122.105"
