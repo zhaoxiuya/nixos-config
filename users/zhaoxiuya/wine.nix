20881395fd38289da5bd9a40c4a751e1efae5c16
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-28 18:24:16 +0900

    wine

--- users/zhaoxiuya/wine.nix
+++ users/zhaoxiuya/wine.nix
@@ -3,11 +3,5 @@
 {
   home.packages = with pkgs; [
     wineWowPackages.stable
-    wine
-    (wine.override { wineBuild = "wine64"; })
-    wine64
-    wineWowPackages.staging
-    winetricks
-    wineWowPackages.waylandFull
   ];
 }
