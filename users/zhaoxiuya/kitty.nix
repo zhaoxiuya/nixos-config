3e159857538c5a1fbef0ab69a6ccb8d7303f3685
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-28 17:42:56 +0900

    fmt

--- users/zhaoxiuya/kitty.nix
+++ users/zhaoxiuya/kitty.nix
@@ -4,11 +4,6 @@
   programs.kitty = {
     enable = true;
 
-    #font = {
-    #  name = "Monoid";
-    #  size = 13;
-    #};
-
     settings = {
       cursor_shape = "beam";
       shell = "${pkgs.zsh}/bin/zsh";
