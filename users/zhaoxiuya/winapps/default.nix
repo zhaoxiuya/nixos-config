c35248c8a2e1ff77bd560ec2a1f7c21610b58676
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-28 16:39:39 +0900

    virtual

--- users/zhaoxiuya/winapps/default.nix
+++ users/zhaoxiuya/winapps/default.nix
@@ -14,13 +14,15 @@ in
 
   home.file.".config/winapps/winapps.conf".text = ''
     RDP_USER="zhaoxiuya"
-    RDP_DOMAIN="DESKTOP-MO4TLIC"
     RDP_PASS="1234"
-    RDP_IP="192.168.122.105"
+    RDP_IP=""
     VM_NAME="win10"
     WAFLAVOR="libvirt"
     APP_SCAN_TIMEOUT="120"
     DEBUG="true"
+    MULTIMON="false"
+    AUTOPAUSE="off"
+    AUTOPAUSE_TIME="300"
     FREERDP_COMMAND="xfreerdp"
   '';
 }
