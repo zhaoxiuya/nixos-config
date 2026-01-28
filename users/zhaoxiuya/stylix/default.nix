fa3dc48564f5a47a85a6c1d8ce63f64268754858
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-28 18:56:07 +0900

    gruvbox

--- users/zhaoxiuya/stylix/default.nix
+++ users/zhaoxiuya/stylix/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     autoEnable = true;
     image = ./cern.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml;
 
     fonts = {
       sansSerif = {
