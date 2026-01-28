1e14635e993a07329018cde0560267d4c46b0a6e
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-28 19:03:52 +0900

    /catppuccin-latte.yaml

--- users/zhaoxiuya/stylix/default.nix
+++ users/zhaoxiuya/stylix/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     autoEnable = true;
     image = ./cern.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/github-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-latte.yaml";
 
     fonts = {
       sansSerif = {
